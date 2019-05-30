4f330f3afe666dd816ab25b4cdd45d35586eefc7
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-30 09:25:49 +0200

    Fixed run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,8 +1,8 @@
 echo "Creating new JAR-File..."
 mvn clean install
 echo "Removing current container..."
-docker container rm -f planing
+docker container rm -f inputadapter
 echo "Removing current image..."
-docker image rm -f planing
+docker image rm -f inputadapter
 echo "Building new image and starting container..."
 docker-compose up -d
