58d1d1962f67b7ba9587b94a7cda10451428841b
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-30 12:53:15 +0200

    Using logger instead of System.out

--- src/main/java/de/smartheating/inputAdapter/rabbitmq/MessageConsumer.java
+++ src/main/java/de/smartheating/inputAdapter/rabbitmq/MessageConsumer.java
@@ -1,13 +1,17 @@
 package de.smartheating.inputAdapter.rabbitmq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import de.smartheating.SmartHeatingCommons.persistedData.Event;
 
 @Component
 public class MessageConsumer {
+	
+	Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
 
 	public void consumeEvent(Event event) {
-		System.out.println("Incoming event with value: " + event.getValue());
+		logger.info("Consumed event with id: " + event.getId());
 	}
 }
--- src/main/java/de/smartheating/inputAdapter/rabbitmq/MessageProducer.java
+++ src/main/java/de/smartheating/inputAdapter/rabbitmq/MessageProducer.java
@@ -1,5 +1,7 @@
 package de.smartheating.inputAdapter.rabbitmq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,11 +12,13 @@ import de.smartheating.inputAdapter.configuration.RabbitMQConfig;
 @Component
 public class MessageProducer {
 	
+	Logger logger = LoggerFactory.getLogger(MessageProducer.class);
+	
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
 	public void sendEvent(Event event) {
+		logger.info("Published event to rabbitmq");
 		rabbitTemplate.convertAndSend(RabbitMQConfig.RABBITMQ_EXCHANGE, RabbitMQConfig.RABBITMQ_ROUTINGKEY, event);
-		System.out.println("Published event to rabbitmq");
 	}
 }
