8e1bb929a2b06597e774f382b6554402ed7abac0
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-06-09 10:34:55 +0200

    Changed communication with Repository

--- src/main/java/de/smartheating/inputAdapter/rest/InputController.java
+++ src/main/java/de/smartheating/inputAdapter/rest/InputController.java
@@ -10,53 +10,58 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 
+import de.smartheating.SmartHeatingCommons.communication.DiscoveryService;
 import de.smartheating.SmartHeatingCommons.communication.RepositoryClient;
-import de.smartheating.SmartHeatingCommons.exceptions.ProfileNotSetException;
 import de.smartheating.SmartHeatingCommons.persistedData.Device;
 import de.smartheating.SmartHeatingCommons.persistedData.Event;
 import de.smartheating.inputAdapter.rabbitmq.MessageProducer;
 import io.swagger.annotations.ApiOperation;
+import javassist.NotFoundException;
 
 @RestController
 public class InputController {
 
 	Logger logger = LoggerFactory.getLogger(InputController.class);
-	
+
 	@Autowired
 	RepositoryClient repoClient;
 	@Autowired
 	MessageProducer producer;
+	@Autowired
+	DiscoveryService discoveryService;
 
 	@PostMapping(value = "/device", produces = "application/json")
 	@ApiOperation(value = "This event prepares a device for usage")
 	public ResponseEntity<?> prepareDevice(@RequestBody Device device) {
 		try {
 			logger.info("Got request to prepare a new device with the name: " + device.getDeviceName());
-			Device preparedDevice = repoClient.addDevice(device);
+			Device preparedDevice = repoClient
+					.addDevice(discoveryService.getServiceInstance("repository").getUri().toString(), device);
 			return new ResponseEntity<>(preparedDevice.getId(), HttpStatus.OK);
 		} catch (RestClientException e) {
 			logger.error("Connection with Repository-Service failed");
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (ProfileNotSetException e) {
-			logger.error("Spring-Profile not set");
+		} catch (NotFoundException e) {
+			logger.error(e.getMessage());
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 	}
-	
+
 	@PostMapping(value = "/event", produces = "application/json")
 	@ApiOperation(value = "This endpoint prepares an incoming event for processng")
 	public ResponseEntity<?> prepareEvent(@RequestBody Event event) {
 		try {
 			logger.info("Got request to prepare an event for processing");
-			Event preparedEvent = repoClient.addEvent(event);
+			Event preparedEvent = repoClient
+					.addEvent(discoveryService.getServiceInstance("repository").getUri().toString(), event);
 			producer.sendEvent(preparedEvent);
 			return new ResponseEntity<>(HttpStatus.OK);
 		} catch (RestClientException e) {
 			logger.error("Connection with Repository-Service failed");
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (ProfileNotSetException e) {
-			logger.error("Spring-Profile not set");
+		} catch (NotFoundException e) {
+			logger.error(e.getMessage());
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
